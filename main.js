c3e82827305f19b2e1bcf9fe7c76c3f5dade3246
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-07-06 22:33:35 -0500

    add pretty button indicators

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var socket = io.connect('http://localhost:3000');
 
+var buttons = {
+	38: "up",
+	37: "left",
+	40: "down",
+	39: "right",
+	17: "select",
+	13: "start",
+	88: "a",
+	90: "b"
+};
+
 socket.on('hello', function(data) {
   socket.emit('ready', { ready: true });
   console.log("HANDSHAKE!");
@@ -8,9 +19,11 @@ socket.on('hello', function(data) {
 socket.on('keyUp', function (data) {
   console.log("keyUp", data);
   nes.keyboard.keyUp({keyCode: data});
+  $('#showbutton-'+buttons[data]).css('color', '#777');
 });
 
 socket.on('keyDown', function (data) {
   console.log("keyDown", data);
   nes.keyboard.keyDown({keyCode: data});
+  $('#showbutton-'+buttons[data]).css('color', '#fff');
 });
